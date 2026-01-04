f975814c854424fe9c12b0591d139ab85e28351a
SebasDev1911 <sebastiantriana1911@gmail.com>
2026-01-03 19:37:48 -0500

    Add hello world example using String variable

---
+++ src/introduction/HelloWorld.java
@@ -0,0 +1,13 @@
+package introduction;
+
+// As a rule, all classes in Java are written using CamelCase notation
+public class HelloWorld {
+    public static void main(String [] args){
+        // Variable type String
+        String message = "Hello world";
+
+        // Output for console
+        System.out.println("Input for windows: Hello world");
+        System.out.print(message);
+    }
+}
