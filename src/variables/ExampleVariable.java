d66e52d85cd5a283afc7c0b891ec929f8f90efb4
SebasDev1911 <sebastiantriana1911@gmail.com>
2026-01-03 20:49:02 -0500

    Add some example of variable type

---
+++ src/variables/ExampleVariable.java
@@ -0,0 +1,25 @@
+package variables;
+
+import java.util.Locale;
+
+public class ExampleVariable {
+    public static void main(String[] args) {
+        // Not primitive data type, this is a class whose instances have methods
+        String message = "Hello world";
+
+        // Method of instance for class not primitive String
+        System.out.println(message+" - "+message.toUpperCase());
+
+        int variablePrimitive = 10; // It has not method
+        Integer variableNotPrimitive = 10; // It has methods
+
+        boolean value = false; // True - False
+
+        // Conditional
+        if (value){
+            System.out.println(variablePrimitive);
+        }else{
+            System.out.println(variableNotPrimitive.doubleValue());
+        }
+    }
+}
